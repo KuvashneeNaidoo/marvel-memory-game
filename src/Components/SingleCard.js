5f1e34e31a6eade94fa5c83ba6a062f72b514829
Kuvashnee Naidoo <105747929+KuvashneeNaidoo@users.noreply.github.com>
2025-01-04 21:11:23 +0200

    Update SingleCard.js

--- src/Components/SingleCard.js
+++ src/Components/SingleCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "../CSS/SingleCard.css";
 
 /* We will start creating the SingleCard component by specifying the following card properties below. */
@@ -12,40 +12,20 @@ const SingleCard = ({
   width,
   disabled,
 }) => {
-  const [isFlipped, setIsFlipped] = useState(flipped);
-
-  useEffect(() => {
-    if (isFlipped) {
-      const timer = setTimeout(() => {
-        setIsFlipped(false);
-      }, 3000);
-
-      return () => clearTimeout(timer); // Clean up the timer when the component unmounts
-    }
-  }, [isFlipped]);
-
-  // Handle the click and flip the card
-  const handleCardClick = () => {
-    if (!disabled) {
-      handleClick(id);
-      setIsFlipped(true);
-    }
-  };
-
   /* We will now return these properties. The SingleCard is assigned a className of flipped whereby
-  the card will be flipped over when clicked or matched. It is also assigned a ClassName of flipped with an 
-  empty string which will not allow the card to be flipped over. An "onClick" function is used whereby if 
-  the disabled state is set to true the card will not flip, however if the disabled state is not true then 
-  the handleClick method comes into action. Lastly, the front image of the card will be showed if the 
-  player flips over or matches a card pair, if not then the back image of the card will be shown. */
+the card will be flipped over when clicked or matched. It is also assigned a ClassName of flipped with an 
+empty string which will not allow the card to be flipped over. An "onClick" function is used whereby if 
+the disabled state is set to true the card will not flip, however if the disabled state is not true then 
+the handleClick method comes into action. Lastly, the front image of the card will be showed if the 
+player flips over or matches a card pair, if not then the back image of the card will be shown. */
   return (
     <div
-      className={`flip-container ${isFlipped ? "flipped" : ""}`}
+      className={`flip-container ${flipped ? "flipped" : ""}`}
       style={{
         width,
         height,
       }}
-      onClick={handleCardClick}
+      onClick={() => (disabled ? null : handleClick(id))}
     >
       <div className="flipper">
         <img
@@ -53,14 +33,15 @@ const SingleCard = ({
             height,
             width,
           }}
-          className={isFlipped ? "front" : "back"}
-          src={isFlipped || solved ? `/img/${item}.jpg` : "/img/cardback.jpg"}
+          className={flipped ? "front" : "back"}
+          src={flipped || solved ? `/img/${item}.jpg` : "/img/cardback.jpg"}
           alt={item}
-          loading="lazy"
+          loading="lazy" // Lazy load the image
         />
       </div>
     </div>
   );
 };
 
+/*We export 'SingleCard' to the 'GameBoard' component in order to display this code.*/
 export default SingleCard;
